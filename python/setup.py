dbb2399f7ef11f15c3ce68fe40b1abf5184517f3
Ralf Mueller <stark.dreamdetective@gmail.com>
2013-04-04 11:07:33 +0200

    Add short notice about python implementation

---
+++ python/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup (name   = 'jobqueue',
+  version     = '0.0.1',
+  author      = "Ralf Mueller",
+  author_email= "stark.dreamdetective@gmail.com",
+  license     = "BSD",
+  description = """Queues with a user definied number of working threads """,
+  py_modules  = ["jobqueue"],
+  url         = "https://github.com/Try2Code/jobQueue",
+  classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+    ],
+  )
