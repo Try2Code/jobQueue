feadb64c9cd5aa32152f7045e784c29cbf49f447
Ralf Mueller <ralf.mueller@zmaw.de>
2016-01-29 15:05:14 +0100

    try to use Array for holding the procs instead of Queue, but that fails

--- ruby/lib/parallel_queue.rb
+++ ruby/lib/parallel_queue.rb
@@ -1,6 +1,6 @@
 require 'thread'
 require 'parallel'
-
+require 'pp'
 include Parallel::ProcessorCount
 
 class Queue
@@ -22,6 +22,36 @@ class Queue
           task[0].send(task[1],*task[2..-1])
         end
       else
+        task[0].call
+      end
+    }
+  end
+end
+
+class Array
+  alias :qpush :push
+  def push (*item, &block)
+    qpush(item   ) unless item.empty?
+    qpush([block]) unless block.nil?
+  end
+  def run(workers=processor_count)
+    qpush(Parallel::Stop)
+    Parallel.map(self,:in_threads => workers) {|task|
+#     pp task.class
+#     pp task.respond_to?(:size)
+      puts " #{task.size} "
+      if task.size > 1
+        puts " EINS "
+        if task[0].kind_of? Proc
+          # Expects proc/lambda with arguments, e.g. [mysqrt,2.789]
+          task[0].call(*task[1..-1])
+        else
+          # expect an object in task[0] and one of its methods with arguments in task[1] as a symbol
+          # e.g. [a,[:attribute=,1]
+          task[0].send(task[1],*task[2..-1])
+        end
+      else
+        puts " WASANDERES "
         task[0].call
       end
     }
--- ruby/test/jobQueue-vs-Parallel.rb
+++ ruby/test/jobQueue-vs-Parallel.rb
@@ -2,59 +2,10 @@ require 'benchmark'
 require 'benchmark/ips'
 require 'pp'
 require 'tempfile'
+require 'cdo_lib'
 $:.unshift File.join(File.dirname(__FILE__),"..","lib")
 require 'jobqueue'
 require 'parallel_queue'
-
-def jq
-  q = JobQueue.new(10)
-  100.times {|i|
-    q.push {
-      Math.sqrt(i**2 + 1)
-    }
-  }
-  q
-end
-def pq
-  q = Queue.new
-  100.times {|i|
-    q.push {
-      Math.sqrt(i**2 + 1)
-    }
-  }
-  q
-end
-
-def jq_with_results
-  results = []
-  lock = Mutex.new
-  q = JobQueue.new(10)
-  100.times {|i|
-    q.push {
-      r = Math.sqrt(i**2 + 1)
-      lock.synchronize{ results << r}
-    }
-  }
-  [q,results]
-end
-def pq_with_results
-  results = []
-  q = Queue.new
-  lock = Mutex.new
-  100.times {|i|
-    q.push {
-      r = Math.sqrt(i**2 + 1)
-      lock.synchronize{ results << r}
-    }
-  }
-  [q,results]
-end
-
-_jq = jq
-_pq = pq
-
-_jqR, jR = jq_with_results
-_pqR, pR = pq_with_results
 
 Benchmark.ips do |x|
   x.report('JobQueue.run') { _jq.run }
@@ -64,34 +15,47 @@ Benchmark.ips do |x|
   x.compare!
 end if false
 
-n=500000
+def doIO
+  #Cdo.topo
+end
+n=99999
 nworker = 10
-jq = JobQueue.new(nworker)
-qp = Queue.new
+jQ = JobQueue.new(nworker)
+pQ = Queue.new
+aQ = Array.new
 
-n.times {|i| 
-    jq.push { 
-#     file = Tempfile.new; file.close; file.unlink
-      Math.sin((i**3).to_f)
+    n.times {|i| 
+      pQ.push { 
+        Math.sin((i**3).to_f);
+      }
     }
-}
-n.times {|i| 
-    pq.push { 
-#     file = Tempfile.new; file.close; file.unlink
-      Math.sin((i**3).to_f);
+    n.times {|i| 
+      aQ.push { 
+        Math.sin((i**3).to_f);
+      }
     }
-}
-
+    n.times {|i| 
+      jQ.push { 
+        Math.sin((i**3).to_f)
+      }
+    }
 input = (0..n).to_a
 Benchmark.bm do |x|
-  x.report("JobQueue     :") {     jq.run }
-
-  x.report("ParallelQueue:") {     pq.run(nworker) }
-
   x.report("Parallel     :") { 
     r = Parallel.map(input,:in_threads => nworker) {|i|
-#     file = Tempfile.new; file.close; file.unlink
       Math.sin((i**3).to_f)
-    }
+    } 
   }
+
+  x.report("ParallelQueue:") {     
+    r = pQ.run(nworker)
+  }
+
+  x.report("JobQueue     :") {     
+    jQ.run
+  }
+
+ #x.report("ParallelArray:") {     
+ #  r = aQ.run(nworker)
+ #}
 end
