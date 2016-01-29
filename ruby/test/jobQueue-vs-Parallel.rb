eedc5b54089235a7f57242b19868cddbe5f40c04
Ralf Mueller <ralf.mueller@zmaw.de>
2016-01-29 15:16:03 +0100

    remove Array-based implementation; cleanup benchmark - value are vary a lot accross multiple runs

--- ruby/test/jobQueue-vs-Parallel.rb
+++ ruby/test/jobQueue-vs-Parallel.rb
@@ -1,45 +1,29 @@
 require 'benchmark'
-require 'benchmark/ips'
-require 'pp'
-require 'tempfile'
-require 'cdo_lib'
-$:.unshift File.join(File.dirname(__FILE__),"..","lib")
 require 'jobqueue'
+$:.unshift File.join(File.dirname(__FILE__),"..","lib")
 require 'parallel_queue'
 
-Benchmark.ips do |x|
-  x.report('JobQueue.run') { _jq.run }
-  x.report('Queue.run') { _pq.run(10) }
-  x.report('JobQueue.run - with results') { _jqR.run }
-  x.report('Queue.run - with results') { _pqR.run(10) }
-  x.compare!
-end if false
+n         = 199999
+nworker   = 4
+jQ        = JobQueue.new(nworker)
+pQ        = Queue.new
+resultsJQ = []
+lock      = Mutex.new
 
-def doIO
-  #Cdo.topo
-end
-n=99999
-nworker = 10
-jQ = JobQueue.new(nworker)
-pQ = Queue.new
-aQ = Array.new
+n.times {|i|
+  pQ.push {
+    Math.sin((i**3).to_f);
+  }
+}
+n.times {|i|
+  jQ.push {
+    r = Math.sin((i**3).to_f)
+    lock.synchronize { resultsJQ << r}
+  }
+}
 
-    n.times {|i| 
-      pQ.push { 
-        Math.sin((i**3).to_f);
-      }
-    }
-    n.times {|i| 
-      aQ.push { 
-        Math.sin((i**3).to_f);
-      }
-    }
-    n.times {|i| 
-      jQ.push { 
-        Math.sin((i**3).to_f)
-      }
-    }
 input = (0..n).to_a
+
 Benchmark.bm do |x|
   x.report("Parallel     :") { 
     r = Parallel.map(input,:in_threads => nworker) {|i|
@@ -55,7 +39,5 @@ Benchmark.bm do |x|
     jQ.run
   }
 
- #x.report("ParallelArray:") {     
- #  r = aQ.run(nworker)
- #}
+# puts resultsJQ[0,10].map(&:to_s).join
 end
